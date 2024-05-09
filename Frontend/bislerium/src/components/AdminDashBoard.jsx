a5f998e9be3b7cf438babb2b372f0d911e95542f
anugrah797 <anugrahpudasai61@gmail.com>
2024-05-09 22:50:46 +0545

    AdminPanel UI

---
+++ Frontend/bislerium/src/components/AdminDashBoard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AdminDashBoard() {
+  return (
+    <div></div>
+  )
+}
+
+export default AdminDashBoard
---
+++ Frontend/bislerium/src/components/Top.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Top = () => {
+  return (
+    <div>Top Posts</div>
+  )
+}
+
+export default Top
