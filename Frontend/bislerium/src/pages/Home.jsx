1410f28ceebc1b6becb215f597c9ce26daf5b12e
Niiischal <nischal.khatiwada745@gmail.com>
2024-05-09 18:28:25 +0545

    merge problem solved

--- Frontend/bislerium/src/pages/Home.jsx
+++ Frontend/bislerium/src/pages/Home.jsx
@@ -1,19 +1,11 @@
-<<<<<<< HEAD
+
 import React from 'react'
 import Navbar from "../components/Navbar"
-=======
-import React from "react";
-import Navbar from "../components/Navbar";
->>>>>>> 3756519fbef0081af1af4196f201fb7c5cd5556a
+
 
 function Home() {
   return (
     <div>
-<<<<<<< HEAD
-      <Navbar/>
-=======
-      <Navbar />
->>>>>>> 3756519fbef0081af1af4196f201fb7c5cd5556a
       HOmeeeeeeeeeeeeeeeeeeeeeeeee
     </div>
   );
